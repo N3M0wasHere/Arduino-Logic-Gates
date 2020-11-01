6763916530ec799bfd7780cc0a1e823dc586cd4f
N3M0wasHere <69675291+N3M0wasHere@users.noreply.github.com>
2020-11-02 07:01:21 +0800

    Add files via upload

---
+++ orgate.ino
@@ -0,0 +1,27 @@
+const int out = 10;
+const int in1 = 13;
+const int in2 = 12;
+const int in3 = 11;
+int in1State = 0;
+int in2State = 0; 
+int in3State = 0;
+
+void setup() {
+  pinMode(out,OUTPUT);
+  pinMode(in1,INPUT);
+  pinMode(in2,INPUT);
+  pinMode(in3,INPUT);
+}
+
+void loop() {
+  in1State = digitalRead(in1);
+  in2State = digitalRead(in2);
+  in3State = digitalRead(in3);
+
+  if(in1State == HIGH || (in2State == HIGH || (in3State == HIGH))){
+    digitalWrite(out,HIGH);
+    }
+  else{
+    digitalWrite(out,LOW);
+    }
+}
